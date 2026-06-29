124b446bf04744f5bfb73aa3a110b49b8c1199e5
shashavibez <shalomadama38@gmail.com>
2026-06-29 11:20:01 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,41 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	err := ValidateArgs(os.Args[1:])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	// validate arguments
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		log.Fatal("usage: go run <string> <Banner style(optional)>")
+		return
 	}
 
 	input := os.Args[1]
 
+	// default banner is standard
 	bannerFile := "standard.txt"
 	if len(os.Args) == 3 {
-		bannerFile = os.Args[2] + ".txt"
+		// if banner argument does not end with ".txt", add it
+		if !(strings.HasSuffix(os.Args[2], ".txt")) {
+			bannerFile = os.Args[2] + ".txt"
+		} else {
+			// use banner argument if it ends with ".txt"
+			bannerFile = os.Args[2]
+		}
+
 	}
 
+	// load the banner file
 	charMap, err := LoadBanner(bannerFile)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error loading banner: %v\n", err)
 		os.Exit(1)
 	}
 
-	err = ValidateChars(input, charMap)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
+	// handle empty input
 	if input == "" {
 		fmt.Println()
 		os.Exit(0)
 	}
 
-	
-	output := Generate(input, charMap)
+	// generate and print the ASCII art
+	output := GenerateArt(input, charMap)
 	fmt.Print(output)
 }
